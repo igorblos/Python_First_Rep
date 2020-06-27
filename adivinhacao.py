a671c1720b6ace50749bb8dded7796af1c641176
Igor <igorblosgressler.com>
2020-06-27 16:23:31 -0300

    Adição de comparações

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,24 @@
+print("***********************************")
 print("Bem vindo ao jogo de adivinhação!!!")
+print("***********************************")
+
+numero_secreto = 42
+
+numero = int(input("Digite o seu número: "))
+
+print("Você digitou ", numero)
+
+acertou = numero == numero_secreto
+maior   = numero > numero_secreto
+menor   = numero < numero_secreto
+
+
+if (acertou):
+    print("Você acertou")
+else:
+    if(maior):
+        print("Você errou, seu chute foi maior que o número secreto")
+    elif(menor):
+        print("Você errou, seu chute foi menor que o número secreto")
+
+print("Fim do jogo")
