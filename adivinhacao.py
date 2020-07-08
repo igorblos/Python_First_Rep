b05ebbce4fa48655b813ef18a38ab5e64dceef4c
Igor <igorblosgressler.com>
2020-07-08 08:27:53 -0300

    Aula 08/07/2020

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,3 +1,5 @@
+import random
+
 print("***********************************")
 print("Bem vindo ao jogo de adivinhação!!!")
 print("***********************************")
@@ -5,12 +7,16 @@ print("***********************************")
 numero_secreto = 42
 total_de_tentativas = 3
 
-
 for rodada in range(1,total_de_tentativas+1):
     print("Tentativa {0} rodada {1}".format(rodada,total_de_tentativas))
     numero = int(input("Digite o seu número: "))
 
     print("Você digitou ", numero)
+
+    if(numero<1 or numero>100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
 
     acertou = numero == numero_secreto
     maior   = numero > numero_secreto
@@ -19,6 +25,7 @@ for rodada in range(1,total_de_tentativas+1):
 
     if (acertou):
         print("Você acertou")
+        break
     else:
         if(maior):
             print("Você errou, seu chute foi maior que o número secreto!")
